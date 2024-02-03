2fd46508f2109a32d1b04847837bcd8fc74c1e5c
gurvinder rehal <gurvinderrehal93@gmail.com>
2024-02-03 13:21:51 +0530

    adding demo project for spark

---
+++ lib/ConfigReader.py
@@ -0,0 +1,25 @@
+import configparser
+from pyspark import SparkConf
+
+
+# loading the application configs in python dictionary
+
+def get_app_config(env):
+    config = configparser.ConfigParser()
+    config.read("configs/application.INI")
+    app_conf = {}
+    for key, val in config.items(env):
+        app_conf[key] = val
+    return app_conf
+
+
+# loading the pyspark configs and creating a spark conf object
+
+def get_pyspark_config(env):
+    config = configparser.ConfigParser()
+    config.read("config/pyspark.INI")
+    pyspark_conf = SparkConf()
+    print(config.items(env))
+    for key, value in config.items(env):
+        pyspark_conf.set(key, value)
+    return pyspark_conf
